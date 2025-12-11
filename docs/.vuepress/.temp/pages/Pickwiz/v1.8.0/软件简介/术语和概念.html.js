3d4486cf886f6ad4f40614108cef2e3096ab3f2e
DYLiu <aminaliu77@gmail.com>
2025-12-11 17:48:04 +0800

    更新了某篇文档

--- docs/.vuepress/.temp/pages/Pickwiz/v1.8.0/软件简介/术语和概念.html.js
+++ docs/.vuepress/.temp/pages/Pickwiz/v1.8.0/软件简介/术语和概念.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/Administrator/Desktop/my docs/docs/.vuepress/.temp/pages/PickWiz/V1.8.0/软件简介/术语和概念.html.vue"
-const data = JSON.parse("{\"path\":\"/PickWiz/V1.8.0/%E8%BD%AF%E4%BB%B6%E7%AE%80%E4%BB%8B/%E6%9C%AF%E8%AF%AD%E5%92%8C%E6%A6%82%E5%BF%B5.html\",\"title\":\"术语和概念\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"术语和概念\"},\"git\":{},\"filePathRelative\":\"PickWiz/V1.8.0/软件简介/术语和概念.md\"}")
+const data = JSON.parse("{\"path\":\"/PickWiz/V1.8.0/%E8%BD%AF%E4%BB%B6%E7%AE%80%E4%BB%8B/%E6%9C%AF%E8%AF%AD%E5%92%8C%E6%A6%82%E5%BF%B5.html\",\"title\":\"术语和概念\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"术语和概念\"},\"git\":{\"updatedTime\":1765445439000,\"contributors\":[{\"name\":\"DYLiu\",\"username\":\"DYLiu\",\"email\":\"aminaliu77@gmail.com\",\"commits\":1,\"url\":\"https://github.com/DYLiu\"}],\"changelog\":[{\"hash\":\"7722b2a2f10ee224587d8491dfe629a46fa3d2dc\",\"time\":1765445439000,\"email\":\"aminaliu77@gmail.com\",\"author\":\"DYLiu\",\"message\":\"更新了某篇文档\"}]},\"filePathRelative\":\"PickWiz/V1.8.0/软件简介/术语和概念.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/.temp/pages/Pickwiz/v1.8.0/软件简介/软件界面.html.js
+++ docs/.vuepress/.temp/pages/Pickwiz/v1.8.0/软件简介/软件界面.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/Administrator/Desktop/my docs/docs/.vuepress/.temp/pages/PickWiz/V1.8.0/软件简介/软件界面.html.vue"
-const data = JSON.parse("{\"path\":\"/PickWiz/V1.8.0/%E8%BD%AF%E4%BB%B6%E7%AE%80%E4%BB%8B/%E8%BD%AF%E4%BB%B6%E7%95%8C%E9%9D%A2.html\",\"title\":\"软件界面\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"软件界面\"},\"git\":{},\"filePathRelative\":\"PickWiz/V1.8.0/软件简介/软件界面.md\"}")
+const data = JSON.parse("{\"path\":\"/PickWiz/V1.8.0/%E8%BD%AF%E4%BB%B6%E7%AE%80%E4%BB%8B/%E8%BD%AF%E4%BB%B6%E7%95%8C%E9%9D%A2.html\",\"title\":\"软件界面\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"软件界面\"},\"git\":{\"updatedTime\":1765445439000,\"contributors\":[{\"name\":\"DYLiu\",\"username\":\"DYLiu\",\"email\":\"aminaliu77@gmail.com\",\"commits\":2,\"url\":\"https://github.com/DYLiu\"}],\"changelog\":[{\"hash\":\"7722b2a2f10ee224587d8491dfe629a46fa3d2dc\",\"time\":1765445439000,\"email\":\"aminaliu77@gmail.com\",\"author\":\"DYLiu\",\"message\":\"更新了某篇文档\"},{\"hash\":\"010a7e4160a269885fa3ac0e3978a1bcfc7b2cb0\",\"time\":1765437058000,\"email\":\"aminaliu77@gmail.com\",\"author\":\"DYLiu\",\"message\":\"更新了某篇文档\"}]},\"filePathRelative\":\"PickWiz/V1.8.0/软件简介/软件界面.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
